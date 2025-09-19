5435c8c00ac122c66075b7c75a98b99f8759e583
jun637 <jun@catalyze-research.com>
2025-09-19 11:27:24 +0900

    Update AcceptedCredentials.ts

--- xrpl/PermissionedDomains/AcceptedCredentials.ts
+++ xrpl/PermissionedDomains/AcceptedCredentials.ts
@@ -3,7 +3,7 @@ import { Client } from "xrpl"
 async function inspectDomain() {
   const client = new Client("wss://s.devnet.rippletest.net:51233")
   await client.connect()
-  const DOMAIN_ID = "5771BD9BD9B9BC01816103C9E435E54630AFF83B607DC9BCB0005D249857677D"
+  const DOMAIN_ID = "" //이곳에 확인하려는 Domain ID 넣기
   const r = await client.request({ command: "ledger_entry", index: DOMAIN_ID })
   console.log(JSON.stringify(r, null, 2))
   await client.disconnect()
