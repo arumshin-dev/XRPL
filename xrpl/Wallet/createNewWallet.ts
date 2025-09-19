7678da836cc513f5d990453476cce8a996ae5c01
jun637 <jun@catalyze-research.com>
2025-09-19 13:20:31 +0900

    Update createNewWallet.ts

--- xrpl/Wallet/createNewWallet.ts
+++ xrpl/Wallet/createNewWallet.ts
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv'
-import path from 'path'
 import { Wallet } from 'xrpl'
 
 export async function createNewWallet() {
@@ -8,8 +6,8 @@ export async function createNewWallet() {
     const newWallet = Wallet.generate()
     console.log('새 지갑 생성 완료')
     console.log(`주소: ${newWallet.address}`)
-    console.log(`시드: ${newWallet.seed}`)
-    console.log(`공개키: ${newWallet.publicKey}`)
+    console.log(`시드: ${newWallet.seed}`) 
+    console.log(`공개키: ${newWallet.publicKey}`) // 공개키는 주로 트랜잭션 서명/ Multisig(다중서명)에 쓰임
     return {
       wallet: newWallet,
       address: newWallet.address,
